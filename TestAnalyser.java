cf12e5db4e036f86236ffda61ced319eb10250ef
naveeny061 <naveeny061@gmail.com>
2020-11-22 22:06:09 +0530

    Refactor: Test method for Sad mood

--- TestAnalyser.java
+++ TestAnalyser.java
@@ -6,13 +6,9 @@ import org.junit.Assert;
 public class TestAnalyser {
 	@Test
 	public void given_InSadMood_thenShouldBeSadMood() {
-		MoodAnalyser moodAnalyser=new MoodAnalyser();
-			Assert.assertEquals("SAD",moodAnalyser.analyseMood("I am in Sad Mood"));
+		MoodAnalyser moodAnalyser=new MoodAnalyser("I am in Sad Mood");
+			Assert.assertEquals("SAD",moodAnalyser.analyseMood());
 	}
-	@Test
-	public void given_InHappyMood_thenShouldBeHappyMood() {
-		MoodAnalyser moodAnalyser=new MoodAnalyser();
-			Assert.assertEquals("HAPPY",moodAnalyser.analyseMood("I am in any Mood"));
-	}
+	
 
 }
