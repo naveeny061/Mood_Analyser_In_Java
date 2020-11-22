278cbd632ff6d11e762a2752ba44c29c05470ef7
naveeny061 <naveeny061@gmail.com>
2020-11-22 23:07:19 +0530

    Added: Test method to show message for null mood

--- TestAnalyser.java
+++ TestAnalyser.java
@@ -6,18 +6,34 @@ import org.junit.Assert;
 public class TestAnalyser {
 	@Test
 	public void given_InSadMood_thenShouldBeSadMood() {
-		MoodAnalyser moodAnalyser=new MoodAnalyser("I am in Sad Mood");
-			Assert.assertEquals("SAD",moodAnalyser.analyseMood());
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
+		try {
+			Assert.assertEquals("SAD", moodAnalyser.analyseMood());
+		} catch (MoodAnalysisException e) {
+			e.printStackTrace();
+		}
 	}
+
 	@Test
 	public void given_InHappyMood_thenShouldBeHappyMood() {
-		MoodAnalyser moodAnalyser=new MoodAnalyser("I am in any Mood");
-			Assert.assertEquals("HAPPY",moodAnalyser.analyseMood());
+		MoodAnalyser moodAnalyser = new MoodAnalyser("I am in any Mood");
+		try {
+			Assert.assertEquals("HAPPY", moodAnalyser.analyseMood());
+		} catch (MoodAnalysisException e) {
+
+			e.printStackTrace();
+		}
 	}
+
 	@Test
-	public void given_InNullMood_thenShouldBeHappyMood() {
-		MoodAnalyser moodAnalyser=new MoodAnalyser(null);
-			Assert.assertEquals("HAPPY",moodAnalyser.analyseMood());
+	public void given_InNullMood_thenShouldThrowMoodAnalysisException() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalysisException e) {
+			Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.getType());
+			e.printStackTrace();
+		}
 	}
 
 }
