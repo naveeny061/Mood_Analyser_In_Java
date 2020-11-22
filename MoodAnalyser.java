19ed84a2735b75261244d083623a2ec183c65452
naveeny061 <naveeny061@gmail.com>
2020-11-22 22:15:40 +0530

    Added: Try and Catch to handle exception

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -2,17 +2,24 @@ package com.assigments;
 
 public class MoodAnalyser {
 	String message;
+
 	public MoodAnalyser() {
 		// TODO Auto-generated constructor stub
 	}
-	public MoodAnalyser(String message) {
-		this.message=message;
-	}
-	public String analyseMood() {
-		if (message.contains("I am in Sad Mood"))
-			return "SAD";
-		else
-			return "HAPPY";
 
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	public String analyseMood() {
+		try {
+			if (message.contains("I am in Sad Mood"))
+				return "SAD";
+			else
+				return "HAPPY";
+
+		} catch (NullPointerException e) {
+				return null;
+		}
 	}
 }
