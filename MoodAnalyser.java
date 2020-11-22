39b78c5dc2d399107e854abdaf4648bc918bfbe2
naveeny061 <naveeny061@gmail.com>
2020-11-22 20:38:09 +0530

    Added: A method to analyse mood

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -1,7 +1,11 @@
 package com.assigments;
 
 public class MoodAnalyser {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Mood Analyser Problem");
+	public String analyseMood(String message) {
+		if (message.contains("Sad"))
+			return message;
+		else
+			return message;
+
 	}
 }
